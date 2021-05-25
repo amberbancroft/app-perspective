a5cd51a758717a49f19676c58b55b2a3c6557305
Amber <amberbancroft2@gmail.com>
2021-05-25 13:51:44 -0700

    test

--- frontend/src/components/SignedHomePage/index.js
+++ frontend/src/components/SignedHomePage/index.js
@@ -14,7 +14,8 @@ function Home(){
 	// importing the reducer
 	const photosList = useSelector(state => state.photos);
 	// const firstPhoto = photosList[1].imgUrl;
-	console.log(photosList[1]);
+	// console.log(photosList[1].imgUrl);
+	
 	
 	// call built in hooks to redirect and sends the updates
 	const dispatch = useDispatch();
@@ -26,22 +27,14 @@ function Home(){
 		dispatch(getPhotos());
 	}, [dispatch]);
 
-	// function randomImage() {
-	// 	photosList
-	// 	const index = Math.floor(Math.random() * images.length);
-	// 	const finalphoto = images[index]
-	// 	console.log(finalphoto);
-	// 	document.body.style.background = `url(${finalphoto})`;
-	// 	return finalphoto;
-	// }
-
-	// const entries = Object.entries(photosList)
-	// console.log(entries)
 
 	return (
 		<div className="img-list">
-			{/* <img src={firstPhoto} alt='house'></img> */}
-			
+			{Object.values(photosList).map(photo => {
+				return (
+					<img className="individual-photo" src={photo.imgUrl} alt={`${photo.title}`}/>
+				)
+			})}
 		</div>
 	);
 };
